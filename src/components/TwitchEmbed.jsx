fdcc02eb72951f712a539d6f137dc4643482c690
christyansegecs <christyansegecs@hotmail.com>
2023-08-29 17:14:38 -0300

    fixThreeFour

--- src/components/TwitchEmbed.jsx
+++ src/components/TwitchEmbed.jsx
@@ -9,7 +9,7 @@ export function TwitchEmbed() {
       <ReactTwitchEmbedVideo channel="talk2megooseman" />
     </div>
     <div>
-      <iframe src="https://clips.twitch.tv/embed?clip=FreezingIgnorantTardigradeYee-JQbeCETm8SnuQqs5&parent=https://christyansegecs.github.io" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+      <iframe src="https://clips.twitch.tv/embed?clip=FreezingIgnorantTardigradeYee-JQbeCETm8SnuQqs5&parent=christyansegecs.github.io" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
     </div>      
     </>
   );
