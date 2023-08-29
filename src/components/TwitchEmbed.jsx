292a81ce1072abe8cd774b082777f8e81d65f853
christyansegecs <christyansegecs@hotmail.com>
2023-08-29 16:41:22 -0300

    fixeight

--- src/components/TwitchEmbed.jsx
+++ src/components/TwitchEmbed.jsx
@@ -6,7 +6,7 @@ export function TwitchEmbed() {
   return (
     <>
     <div>
-      <ReactTwitchEmbedVideo channel="dotalobbysquad" />
+      <ReactTwitchEmbedVideo channel="talk2megooseman" />
     </div>
     <div>
       <iframe src="https://clips.twitch.tv/embed?clip=FreezingIgnorantTardigradeYee-JQbeCETm8SnuQqs5&parent=https://christyansegecs.github.io/Twitch-Embed&output=embed" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
