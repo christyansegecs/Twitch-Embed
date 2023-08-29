b39c5192e9fafcf8d6e262f0d6b59ffc85790676
christyansegecs <christyansegecs@hotmail.com>
2023-08-29 16:38:26 -0300

    fixseven

--- src/components/TwitchEmbed.jsx
+++ src/components/TwitchEmbed.jsx
@@ -5,8 +5,12 @@ export function TwitchEmbed() {
 
   return (
     <>
-      <iframe src="https://clips.twitch.tv/embed?clip=FreezingIgnorantTardigradeYee-JQbeCETm8SnuQqs5&parent=https://christyansegecs.github.io/Twitch-Embed&output=embed" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+    <div>
       <ReactTwitchEmbedVideo channel="dotalobbysquad" />
+    </div>
+    <div>
+      <iframe src="https://clips.twitch.tv/embed?clip=FreezingIgnorantTardigradeYee-JQbeCETm8SnuQqs5&parent=https://christyansegecs.github.io/Twitch-Embed&output=embed" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+    </div>      
     </>
   );
 }
